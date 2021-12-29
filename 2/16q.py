4fea260e273b8886e1fa2520fd81677a134c7d28
Darya <75693374+DaryaNavumchyk@users.noreply.github.com>
2021-12-29 21:49:53 +0300

    16.py

--- 2/16q.py
+++ 2/16q.py
@@ -2,4 +2,19 @@ m=int (input())
 n=int (input())
 k=int (input())
 
+s=m*n
+a=s-n
+b=s-m
 
+
+if k<s:
+    if k==1:
+        print("no")
+    elif k%m==0 or k%n==0:
+        print ("yes")
+    elif a%k==0 or b%k==0:
+        print("yes")
+    else:
+        print("no")
+else:
+    print("no")
